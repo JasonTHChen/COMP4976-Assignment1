4b7bce8bafec7906bb35ac84bbb4ad5799ebecc8
Jason Chen <jasonccmfry@gmail.com>
2018-02-06 14:17:18 -0800

    create boat model and modify user register model
    Signed-off-by: Jason Chen <jasonccmfry@gmail.com>

---
+++ LymcDataLib/Models/Boat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LymcDataLib.Models
+{
+    public class Boat
+    {
+        public int BoatId { get; set; }
+
+        [Display(Name = "Boat Name")]
+        public string BoatName { get; set; }
+        public byte[] Picture { get; set; }
+
+        [Display(Name = "Length In Feet")]
+        public double LengthInFeet { get; set; }
+        public string Make { get; set; }
+        public DateTime Year { get; set; }
+
+        [Display(Name = "Record Creation Date")]
+        public DateTime RecordCreationDate { get; set; }
+
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
